79e2c50880685d86184e38e288c9f0c4cb260307
saltanat2211 <34503@iitu.edu.kz>
2024-12-18 07:44:30 +0500

    update

--- src/main/java/com/example/bookmanagement/model/Book.java
+++ src/main/java/com/example/bookmanagement/model/Book.java
@@ -1,12 +1,15 @@
 package com.example.bookmanagement.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private String title;
@@ -17,32 +20,26 @@ public class Book {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public int getYear() {
         return year;
     }
-
     public void setYear(int year) {
         this.year = year;
     }
+
 }
